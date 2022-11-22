0870d997ef50119c1737801bee59ac817c9fc8e9
LEENSATTA <117806280+LEENSATTA@users.noreply.github.com>
2022-11-22 10:57:42 +0700

    idkwatamdoin1

--- app/src/main/java/com/example/virtualeo/MainActivity.java
+++ app/src/main/java/com/example/virtualeo/MainActivity.java
@@ -2,14 +2,31 @@ package com.example.virtualeo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.buttonLogIn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openhome();
+            }
+        });
+    }
+
+    public void openhome(){
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
     }
 }
